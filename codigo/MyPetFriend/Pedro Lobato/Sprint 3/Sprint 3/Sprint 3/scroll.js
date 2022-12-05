b24b7749a231666ed7c0c11439e4f412b864e401
henriquedani <113548836+henriquedani@users.noreply.github.com>
2022-12-05 18:27:34 -0300

    Delete codigo/MyPetFriend/Pedro Lobato directory

--- codigo/MyPetFriend/Pedro Lobato/Sprint 3/Sprint 3/Sprint 3/scroll.js
+++
@@ -1,9 +0,0 @@
-const nav = document.querySelector('navbar');
-
-        window.addEventListener('scroll', () => {
-            if (window.scrollY >= 0) {
-                nav.classList.add('active_navbar');
-            } else {
-                nav.classList.remove('active_navbar');
-            }
-}) 
